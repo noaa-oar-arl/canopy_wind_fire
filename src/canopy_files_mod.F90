a46841dc05d094c8e774f1274165e249c1b0bcf8
drnimbusrain <dr.nimbusrain@gmail.com>
2024-05-07 20:59:50 -0400

    Increased max possible number of input canopy files.

--- src/canopy_files_mod.F90
+++ src/canopy_files_mod.F90
@@ -10,11 +10,13 @@ MODULE canopy_files_mod
     IMPLICIT NONE
 
     INTEGER                       :: cdfid_m
-    INTEGER,            PARAMETER :: max_mm       = 250
+    INTEGER,            PARAMETER :: max_mm       = 10000
     INTEGER,            PARAMETER :: iutnml       =  8
+
     CHARACTER(LEN=256)            :: file_vars    ( max_mm )
     CHARACTER(LEN=256)            :: file_canvars ( max_mm )
     CHARACTER(LEN=256)            :: file_out     ( 1 )
     CHARACTER(LEN=*), PARAMETER   :: file_nml     = 'input/namelist.canopy'
+
 
 END MODULE canopy_files_mod
