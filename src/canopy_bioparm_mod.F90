86066b026a55f69284f239827e7b720d3228f5ce
Patrick Campbell <dr.nimbusrain@gmail.com>
2024-07-25 12:19:15 -0400

    Update src/canopy_bioparm_mod.F90
    Co-authored-by: Maggie Marvin <47859989+MaggieMarvin@users.noreply.github.com>

--- src/canopy_bioparm_mod.F90
+++ src/canopy_bioparm_mod.F90
@@ -639,7 +639,7 @@ contains
         REAL(RK),          PARAMETER     :: CLT_MONO         =  1.0_rk     !coefficient for low temperature stress
         REAL(RK),          PARAMETER     :: TLT_MONO         =  283.15_rk  !threshold for low temperature stress (K)
         REAL(RK),          PARAMETER     :: DTLT_MONO        =  8.0_rk     !delta threshold for low temperature stress (K)
-        REAL(RK),          PARAMETER     :: CHW_MONO         =  1.0_rk     !coefficient for high wind stress
+        REAL(RK),          PARAMETER     :: CHW_MONO         =  5.0_rk     !coefficient for high wind stress
         REAL(RK),          PARAMETER     :: THW_MONO         =  12.0_rk    !threshold for high wind stress (m/s)
         REAL(RK),          PARAMETER     :: DTHW_MONO        =  8.0_rk     !delta threshold for high wind stress (m/s)
 
